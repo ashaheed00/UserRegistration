ea53576e0384966eaf134bb85a304a22344cc410
ashaheed00 <ashaheed00@gmail.com>
2020-09-30 19:46:02 +0530

    Refactor >> UC10

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,41 +1,25 @@
 // Version UC10
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class UserRegistration {
 
 	public boolean firstNameValidate(String fName) {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		Matcher matcher = pattern.matcher(fName);
-		return matcher.find();
+		return UserInputPatterns.NAME.matcher(fName).find();
 	}
 
 	public boolean lastNameValidate(String lName) {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		Matcher matcher = pattern.matcher(lName);
-		return matcher.find();
+		return UserInputPatterns.NAME.matcher(lName).find();
 	}
 
 	public boolean emailValidate(String email) {
-		// considering all test cases
-		Pattern pattern = Pattern.compile("^[a-z][0-9a-z_+-]*\\.?[0-9a-z_+-]+@\\w+(\\.[a-z]{2,}\\w*){1,2}$");
-		Matcher matcher = pattern.matcher(email);
-		return matcher.find();
+		return UserInputPatterns.EMAIL.matcher(email).find();
 	}
 
-	public boolean mobileValidate(String mobile) {
-		// assuming that the first mobile number digit shouldn't be 0
-		Pattern pattern = Pattern.compile("^\\d{2} [1-9]\\d{9}$");
-		Matcher matcher = pattern.matcher(mobile);
-		return matcher.find();
+	public boolean mobileNumberValidate(String mobileNUmber) {
+		return UserInputPatterns.MOBILE_NUMBER.matcher(mobileNUmber).find();
 	}
 
 	public boolean passwordValidate(String password) {
-		// Rule4 – has exactly one special character
-		Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.*\\W\\w*\\W)(?!.*\\s).{8,}$");
-		Matcher matcher = pattern.matcher(password);
-		return matcher.find();
+		return UserInputPatterns.PASSWORD.matcher(password).find();
 	}
 
 }

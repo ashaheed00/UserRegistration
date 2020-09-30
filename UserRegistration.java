c6f2024ccb9ff20baecb6d6e2bc6c176a3aa293f
ashaheed00 <ashaheed00@gmail.com>
2020-09-30 19:25:13 +0530

    Refactor >> UC4

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,47 +1,24 @@
 // Version UC4
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
 	public boolean firstNameValidate(String fName) {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		Matcher matcher = pattern.matcher(fName);
-		return matcher.find();
+		return UserInputPatterns.NAME.matcher(fName).find();
 	}
 
 	public boolean lastNameValidate(String lName) {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		Matcher matcher = pattern.matcher(lName);
-		return matcher.find();
+		return UserInputPatterns.NAME.matcher(lName).find();
 	}
 
 	public boolean emailValidate(String email) {
-		Pattern pattern = Pattern.compile("^([a-z][0-9a-z_-]*)(.[a-z][0-9a-z_-]*)?@bl.co(.in)?$");
-		Matcher matcher = pattern.matcher(email);
-		return matcher.find();
+		return UserInputPatterns.EMAIL_BL.matcher(email).find();
 	}
 
-	public boolean mobileValidate(String mobile) {
-		// assuming that the first mobile number digit shouldn't be 0
-		Pattern pattern = Pattern.compile("^\\d{2} [1-9]\\d{9}$");
-		Matcher matcher = pattern.matcher(mobile);
-		return matcher.find();
+	public boolean mobileNumberValidate(String mobileNUmber) {
+		return UserInputPatterns.MOBILE_NUMBER.matcher(mobileNUmber).find();
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		UserRegistration userReg = new UserRegistration();
-
-		// Checking mobile number...
-		System.out.println("Enter mobile number: ");
-		if (userReg.mobileValidate(sc.nextLine().trim()))
-			System.out.println("Valid mobile number.");
-		else
-			System.out.println("Invalid mobile number.");
-
-		sc.close();
-	}
 }

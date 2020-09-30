784db97103ef81103a41b13025c3e23fa57883ed
ashaheed00 <ashaheed00@gmail.com>
2020-09-30 19:07:25 +0530

    Refactor >> UC2

---
+++ UserInputPatterns.java
@@ -0,0 +1,8 @@
+package com.bl.userreg;
+
+import java.util.regex.Pattern;
+
+public class UserInputPatterns {
+	final static Pattern NAME = Pattern.compile("^[A-Z][a-z]{2,}$");
+	
+}
--- UserRegistration.java
+++ UserRegistration.java
@@ -7,28 +7,11 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
 	public boolean firstNameValidate(String fName) {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		Matcher matcher = pattern.matcher(fName);
-		return matcher.find();
-	}
-	
-	public boolean lastNameValidate(String lName) {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		Matcher matcher = pattern.matcher(lName);
-		return matcher.find();
+		return UserInputPatterns.NAME.matcher(fName).find();
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		UserRegistration userReg = new UserRegistration();
-		
-		// Checking last name...
-		System.out.println("Enter last name: ");
-		if (userReg.firstNameValidate(sc.next()))
-			System.out.println("Valid Last Name");
-		else
-			System.out.println("Invalid Last Name Format." + "\nLast name starts with Cap and has min 3 characters");
-		
-		sc.close();
+	public boolean lastNameValidate(String lName) {
+		return UserInputPatterns.NAME.matcher(lName).find();
 	}
+
 }

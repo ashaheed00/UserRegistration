7b77b988fd4d1c1f8534a5bc1b0d99b34f8d7205
ashaheed00 <ashaheed00@gmail.com>
2020-09-30 13:13:44 +0530

    Adding into branch UC13

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,63 +1,13 @@
-// Version UC12
+// Version UC13
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+@FunctionalInterface
+interface ValidateUserInput {
+	boolean validate(String input);
+}
 
 public class UserRegistration {
-	
-	// Patter for first and last name are exactly same. Hence a common method is created.
-	public boolean nameValidate(String name) throws UserDetailsException {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		try {
-			Matcher matcher = pattern.matcher(name);
-			if (matcher.find())
-				return true;
-			else
-				throw new UserDetailsException(UserDetailsException.ExceptionType.INVALID, "Invalid input.");
-		} catch (NullPointerException e) {
-			throw new UserDetailsException(UserDetailsException.ExceptionType.NULL, "Null input.");
-		}
-	}
-
-	public boolean emailValidate(String email) throws UserDetailsException {
-		Pattern pattern = Pattern.compile("^[a-z][0-9a-z_+-]*\\.?[0-9a-z_+-]+@\\w+(\\.[a-z]{2,}\\w*){1,2}$");
-		try {
-			Matcher matcher = pattern.matcher(email);
-			if (matcher.find())
-				return matcher.find();
-			else
-				throw new UserDetailsException(UserDetailsException.ExceptionType.INVALID, "Invalid input.");
-		} catch (NullPointerException e) {
-			throw new UserDetailsException(UserDetailsException.ExceptionType.NULL, "Null input.");
-		}
-	}
-
-	public boolean mobileValidate(String mobile) throws UserDetailsException {
-		// assuming that the first mobile number digit shouldn't be 0
-		Pattern pattern = Pattern.compile("^\\d{2}\\s{1}[1-9]\\d{9}$");
-		try {
-			Matcher matcher = pattern.matcher(mobile);
-			if (matcher.find())
-				return matcher.find();
-			else
-				throw new UserDetailsException(UserDetailsException.ExceptionType.INVALID, "Invalid input.");
-		} catch (NullPointerException e) {
-			throw new UserDetailsException(UserDetailsException.ExceptionType.NULL, "Null input.");
-		}
-	}
-
-	public boolean passwordValidate(String password) throws UserDetailsException {
-		// Rule4 password has exactly one special character
-		Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.*\\W\\w*\\W)(?!.*\\s).{8,}$");
-		try {
-			Matcher matcher = pattern.matcher(password);
-			if (matcher.find())
-				return matcher.find();
-			else
-				throw new UserDetailsException(UserDetailsException.ExceptionType.INVALID, "Invalid input.");
-		} catch (NullPointerException e) {
-			throw new UserDetailsException(UserDetailsException.ExceptionType.NULL, "Null input.");
-		}
-	}
-
+	static ValidateUserInput validateName = (input) -> UserInputPatterns.NAME.matcher(input).find();
+	static ValidateUserInput validateEmail = (input) -> UserInputPatterns.EMAIL.matcher(input).find();
+	static ValidateUserInput validateMobileNumer = (input) -> UserInputPatterns.MOBILE_NUMBER.matcher(input).find();
+	static ValidateUserInput validatePassword = (input) -> UserInputPatterns.PASSWORD.matcher(input).find();
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -8,106 +8,54 @@ import org.junit.Test;
 public class UserRegistrationTest {
 
 	@Test
-	public void testNameValidate() throws UserDetailsException {
-		UserRegistration user = new UserRegistration2();
-		// Null check
-		try {
-			user.nameValidate(null);
-		} catch (UserDetailsException e) {
-
-			assertEquals(UserDetailsException.ExceptionType.NULL, e.exceptionType);
-		}
+	public void testNameValidate() {
 		// Valid names
 		String[] validNames = { "Akram", "Joe" };
 		for (String name : validNames)
-			user.nameValidate(name);
+			assertTrue(UserRegistration.validateName.validate(name));
 		// Invalid names
-		String[] invalidNames = { "kram", "bh" };
-		for (String name : invalidNames) {
-			try {
-				user.nameValidate(name);
-			} catch (UserDetailsException e) {
-				assertEquals(UserDetailsException.ExceptionType.INVALID, e.exceptionType);
-			}
-		}
-
+		String[] invalidNames = { "kram", "bh", "Akr8a" };
+		for (String name : invalidNames)
+			assertFalse(UserRegistration.validateName.validate(name));
 	}
 
 	@Test
-	public void testEmailValidate() throws UserDetailsException {
-		UserRegistration user = new UserRegistration2();
-		// Null check
-		try {
-			user.emailValidate(null);
-		} catch (UserDetailsException e) {
-			assertEquals(UserDetailsException.ExceptionType.NULL, e.exceptionType);
-		}
+	public void testEmailValidate() {
 		// Valid email
 		String[] validEmails = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
 				"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.co", "abc+100@gmail.com" };
 		for (String email : validEmails)
-			user.emailValidate(email);
+			assertTrue(UserRegistration.validateEmail.validate(email));
 		// Invalid email
 		String[] invalidEmails = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
 				".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com",
 				"abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
-		for (String email : invalidEmails) {
-			try {
-				user.emailValidate(email);
-			} catch (UserDetailsException e) {
-				assertEquals(UserDetailsException.ExceptionType.INVALID, e.exceptionType);
-			}
-		}
+		for (String email : invalidEmails)
+			assertFalse((UserRegistration.validateEmail.validate(email)));
 	}
 
 	@Test
-	public void testMobileValidate() throws UserDetailsException {
-		UserRegistration user = new UserRegistration2();
-
-		// Null check
-		try {
-			user.mobileValidate(null);
-		} catch (UserDetailsException e) {
-			assertEquals(UserDetailsException.ExceptionType.NULL, e.exceptionType);
-		}
+	public void testMobileValidate() {
 		// Valid mobile numbers
 		String[] validMobileNumbers = { "91 9851605588", "92 6584122568" };
 		for (String mobileNumber : validMobileNumbers)
-			user.mobileValidate(mobileNumber);
+			assertTrue(UserRegistration.validateMobileNumer.validate(mobileNumber));
 		// Invalid mobile numbers
 		String[] invalidEmails = { "9 9851256598", "91 67656415" };
-		for (String mobileNumber : invalidEmails) {
-			try {
-				user.mobileValidate(mobileNumber);
-			} catch (UserDetailsException e) {
-				assertEquals(UserDetailsException.ExceptionType.INVALID, e.exceptionType);
-			}
-		}
+		for (String mobileNumber : invalidEmails)
+			assertFalse(UserRegistration.validateMobileNumer.validate(mobileNumber));
 	}
 
 	@Test
-	public void testPasswordValidate() throws UserDetailsException {
-		UserRegistration user = new UserRegistration2();
-
-		// Null check
-		try {
-			user.passwordValidate(null);
-		} catch (UserDetailsException e) {
-			assertEquals(UserDetailsException.ExceptionType.NULL, e.exceptionType);
-		}
+	public void testPasswordValidate() {
 		// Valid passwords
 		String[] validPasswords = { "Akram123*", "Akram3Kabir/" };
 		for (String password : validPasswords)
-			user.passwordValidate(password);
+			assertTrue(UserRegistration.validatePassword.validate(password));
 		// Invalid passwords
 		String[] invalidPasswords = { "Akrhg*98/", "AkramKabir!", "AkramKabir!", "AkramKabir!" };
-		for (String password : invalidPasswords) {
-			try {
-				user.passwordValidate(password);
-			} catch (UserDetailsException e) {
-				assertEquals(UserDetailsException.ExceptionType.INVALID, e.exceptionType);
-			}
-		}
+		for (String password : invalidPasswords)
+			assertFalse(UserRegistration.validatePassword.validate(password));
 	}
 
 }

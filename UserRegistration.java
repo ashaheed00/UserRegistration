d7ab69426b984f04adbac6b2dd14058be6788c58
ashaheed00 <ashaheed00@gmail.com>
2020-09-30 19:02:55 +0530

    Refactor >> UC1

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,22 +7,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
 	public boolean firstNameValidate(String fName) {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		Matcher matcher = pattern.matcher(fName);
-		return matcher.find();
+		return UserInputPatterns.NAME.matcher(fName).find();
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		UserRegistration userReg = new UserRegistration();
-
-		// Checking First name...
-		System.out.println("Enter first your name: ");
-		if (userReg.firstNameValidate(sc.next()))
-			System.out.println("Valid Name");
-		else
-			System.out.println("Invalid First Name Format." + "\nFirst name starts with Cap and has minimum 3 characters");
-		
-		sc.close();
-	}
 }

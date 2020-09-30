dddfaff1d2add837325d99f86200a2db063a4736
ashaheed00 <ashaheed00@gmail.com>
2020-09-30 19:32:03 +0530

    Refactor >> UC8

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,54 +1,25 @@
 // Version UC8
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class UserRegistration {
 
 	public boolean firstNameValidate(String fName) {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		Matcher matcher = pattern.matcher(fName);
-		return matcher.find();
+		return UserInputPatterns.NAME.matcher(fName).find();
 	}
 
 	public boolean lastNameValidate(String lName) {
-		Pattern pattern = Pattern.compile("^([A-Z])\\w{2,}$");
-		Matcher matcher = pattern.matcher(lName);
-		return matcher.find();
+		return UserInputPatterns.NAME.matcher(lName).find();
 	}
 
 	public boolean emailValidate(String email) {
-		Pattern pattern = Pattern.compile("^([a-z][0-9a-z_-]*)(.[a-z][0-9a-z_-]*)?@bl.co(.in)?$");
-		Matcher matcher = pattern.matcher(email);
-		return matcher.find();
+		return UserInputPatterns.EMAIL_BL.matcher(email).find();
 	}
 
-	public boolean mobileValidate(String mobile) {
-		// assuming that the first mobile number digit shouldn't be 0
-		Pattern pattern = Pattern.compile("^\\d{2} [1-9]\\d{9}$");
-		Matcher matcher = pattern.matcher(mobile);
-		return matcher.find();
+	public boolean mobileNumberValidate(String mobileNUmber) {
+		return UserInputPatterns.MOBILE_NUMBER.matcher(mobileNUmber).find();
 	}
-	
+
 	public boolean passwordValidate(String password) {
-		// Rule4 – has exactly one special character
-		Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[A-Z])(?=.*\\W)(?!.*\\W\\w*\\W)(?!.*\s).{8,}$");
-		Matcher matcher = pattern.matcher(password);
-		return matcher.find();
+		return UserInputPatterns.PASSWORD.matcher(password).find();
 	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		UserRegistration userReg = new UserRegistration();
-
-		// Checking password...
-		System.out.println("Enter your password: ");
-		if (userReg.passwordValidate(sc.nextLine().trim()))
-			System.out.println("Valid password.");
-		else
-			System.out.println("Invalid password.");
-
-		sc.close();
-	}
 }
